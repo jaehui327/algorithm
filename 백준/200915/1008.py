a315570bdcf43e647a79f617a8c0594f2b2b5302
jaehui327 <jaeheekim970327@gmail.com>
2020-09-15 23:47:25 +0900

    Create 1094-막대기.py

--- 백준/200915/1008.py
+++ 백준/200915/1008.py
@@ -6,5 +6,10 @@
 # 출력
 # 첫째 줄에 A/B를 출력한다. 실제 정답과 출력값의 절대오차 또는 상대오차가 10-9 이하이면 정답이다.
 #
+# a, b = input("").split()
+# a = float(a)
+# b = float(b)
+# print(a / b)
+#
 a,b=map(int,input().split())
 print(a/b)
