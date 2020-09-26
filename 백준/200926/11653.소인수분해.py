a73b246f9dc368db5f5b0d491b54a20e081da5a5
jaehui327 <jaeheekim970327@gmail.com>
2020-09-26 18:15:53 +0900

    feat: 11653 속도 개선
    1164ms -> 64ms

--- 백준/200926/11653.소인수분해.py
+++ 백준/200926/11653.소인수분해.py
@@ -8,11 +8,37 @@
 # 출력
 # N의 소인수분해 결과를 한 줄에 하나씩 오름차순으로 출력한다.
 #
+
+# import sys
+# n=int(sys.stdin.readline())
+# while n>1:
+#     for i in range(2, n+1):
+#         if n%i==0:
+#             print(i)
+#             break
+#     n//=i
+
 import sys
 n=int(sys.stdin.readline())
+x=2
 while n>1:
-    for i in range(2, n+1):
-        if n%i==0:
-            print(i)
-            break
-    n//=i
+    if n%x==0:
+        print(x)
+        n//=x
+    elif x*x>n:
+        print(n)
+        break
+    else: x+=1
+
+# import sys
+# n=int(sys.stdin.readline())
+# x=2
+# r=int(n**0.5)
+#
+# while x<=r:
+#     while not n%x:
+#         print(x)
+#         n//=x
+#     x += 1
+# if n>1:
+#     print(n)
