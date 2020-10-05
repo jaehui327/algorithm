fbfb2b06d3442f3a1d2e99fde6f834dc90ac297e
jaehui327 <jaeheekim970327@gmail.com>
2020-10-05 23:17:54 +0900

    Create 15740.A+B-9.py

---
+++ 백준/201005/15740.A+B-9.py
@@ -0,0 +1,8 @@
+# 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+# 입력
+# 첫째 줄에 A와 B (-1010000 ≤ A, B ≤ 1010000)가 주어진다.
+# 출력
+# 첫째 줄에 A+B를 출력한다.
+import sys
+a, b=map(int, sys.stdin.readline().split())
+print(a+b)
