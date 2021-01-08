122d07718c7e0123fbd704fb227e99e7dbc184ac
jaehui327 <jaeheekim970327@gmail.com>
2021-01-08 12:25:26 +0900

    :sparkles: leetcode 819. Most Common Word

---
+++ leetcode/210108/819. Most Common Word.py
@@ -0,0 +1,33 @@
+#
+# 819. Most Common Word
+#
+# Given a paragraph and a list of banned words, return the most frequent word that is not in the list of banned words.
+# It is guaranteed there is at least one word that isn't banned, and that the answer is unique.
+#
+# Words in the list of banned words are given in lowercase, and free of punctuation.
+# Words in the paragraph are not case sensitive.  The answer is in lowercase.
+#
+# Example:
+#
+# Input:
+# paragraph = "Bob hit a ball, the hit BALL flew far after it was hit."
+# banned = ["hit"]
+#
+# Output: "ball"
+# Explanation:
+# "hit" occurs 3 times, but it is a banned word.
+# "ball" occurs twice (and no other word does), so it is the most frequent non-banned word in the paragraph.
+# Note that words in the paragraph are not case sensitive,
+# that punctuation is ignored (even if adjacent to words, such as "ball,"),
+# and that "hit" isn't the answer even though it occurs more because it is banned.
+#
+import re
+import collections
+
+
+def most_common_word(self, paragraph: str, banned: [str]) -> str:
+    # 소문자, 구두점을 제외하고 banned를 제외한 단어 목록 저장
+    words = [word for word in re.sub(r'[^\w]', ' ', paragraph).lower().split() if word not in banned]
+    counts = collections.Counter(words)
+    # 가장 흔하게 등장하는 단어의 첫 번째 인덱스 리턴
+    return counts.most_common(1)[0][0]
--- leetcode/README.md
+++ leetcode/README.md
@@ -3,11 +3,11 @@
 ### 210107
 
 - [x] [125. Valid Palindrome](https://leetcode.com/problems/valid-palindrome/)
-
 - [x] [344. Reverse String](https://leetcode.com/problems/reverse-string/)
 
 
 ### 210108
 
 - [x] [937. Reorder Data in Log Files](https://leetcode.com/problems/reorder-data-in-log-files/)
+- [x] [819. Most Common Word](https://leetcode.com/problems/most-common-word/)
 
